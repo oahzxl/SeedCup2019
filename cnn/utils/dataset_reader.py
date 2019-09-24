d3bbf0512a82c3ac79f449b0e16ada1d889cdfea
brycezxl <vincentlei@163.com>
2019-09-25 00:03:52 +0800

    improve reader

--- cnn/utils/dataset_reader.py
+++ cnn/utils/dataset_reader.py
@@ -14,6 +14,7 @@ def dataset_reader(train=True, fields=False, process=False):
     else:
         regular_field = Field(batch_first=True)
 
+    #
     train_data_list = ['plat_form', 'biz_type', 'create_time', 'create_hour',
                        'payed_day', 'payed_hour', 'cate1_id', 'cate2_id',
                        'cate3_id', 'preselling_shipped_day',
@@ -22,7 +23,11 @@ def dataset_reader(train=True, fields=False, process=False):
                        'shipped_prov_id', 'shipped_city_id', 'rvcr_prov_name',
                        'rvcr_city_name', 'shipped_day', 'shipped_hour',
                        'got_day', 'got_hour', 'dlved_day', 'dlved_hour']
-    label_list = ['signed_day', 'signed_hour']
+    label_list = ['signed_day', 'signed_hour', 'lgst_company_label',
+                  'warehouse_id_label', 'shipped_prov_id_label',
+                  'shipped_city_id_label', 'shipped_day_label',
+                  'shipped_hour_label', 'got_day_label', 'got_hour_label',
+                  'dlved_day_label', 'dlved_hour_label']
 
     field = []
     for d in train_data_list:
@@ -45,8 +50,10 @@ def dataset_reader(train=True, fields=False, process=False):
         line = f.readline()
         while line:
             items = list(line.split(' '))
-            items[-1] = float(items[-1])
-            items[-2] = float(items[-2])
+            for i in (25, 26, 31, 32, 33, 34, 35, 36):
+                items[i] = float(items[i])
+            for i in (27, 28, 29, 30):
+                items[i] = int(items[i])
 
             examples.append(Example.fromlist(items, field))
             line = f.readline()
@@ -82,8 +89,8 @@ def process_data(train, path, path_store):
                 'dlved_time', 'signed_time'
                 """
 
-                # hidden data in test
-                if not train and i == 12:
+                # skip hidden data in test
+                if i == 12 and not train:
                     # 4 hidden data
                     for j in range(4):
                         tmp_list.append(str(-99))
@@ -91,7 +98,7 @@ def process_data(train, path, path_store):
                     tmp_list.append(items[12] + '_16')
                     tmp_list.append(items[13][:-1] + '_17')
                     # hidden time
-                    for j in range(8):
+                    for j in range(18):
                         tmp_list.append(str(-99))
                     break
 
@@ -129,6 +136,18 @@ def process_data(train, path, path_store):
 
                 else:
                     tmp_list.append(data + '_' + str(i))
+
+            # hidden data label
+            if train:
+                for i in (12, 13, 14, 15):
+                    tmp_list.append(int(items[i]))
+                for i in (18, 19, 20):
+                    if items[i] == '-99':
+                        tmp_list.append(str(-99))
+                        tmp_list.append(str(-99))
+                    else:
+                        tmp_list.append(int(day_difference(start_date, get_day(items[i]))))
+                        tmp_list.append(int(get_hour(items[i])))
 
             with open(path_store, "a+") as txt:
                 context = ''
