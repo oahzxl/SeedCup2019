5efd2a0e20e52a655f428027674e1be2020002c0
brycezxl <vincentlei@163.com>
2019-09-27 22:41:43 +0800

    first cnn

--- first_cnn/modules/cnn.py
+++ first_cnn/modules/cnn.py
@@ -7,11 +7,14 @@ class CNNCell(Module):
     def __init__(self):
         super(CNNCell, self).__init__()
         self.bn1 = nn.BatchNorm2d(1)
-        self.cnn1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=(3, 3), stride=2, padding=0)
-        self.cnn2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), stride=2, padding=0)
-        self.bn2 = nn.BatchNorm2d(128)
-        self.cnn3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3, 3), stride=2, padding=0)
-        self.cnn4 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3, 3), stride=1, padding=0)
+        self.cnn1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=(3, 3), stride=2, padding=1)
+        self.cnn2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), stride=2, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.cnn3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), stride=2, padding=1)
+        self.cnn4 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.cnn5 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3, 3), stride=1, padding=0)
+        self.cnn6 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3, 3), stride=1, padding=0)
 
     def forward(self, inputs):
         inputs = self.bn1(inputs)
@@ -21,5 +24,9 @@ class CNNCell(Module):
         inputs = self.bn2(inputs)
         inputs = self.cnn3(inputs)
         inputs = self.cnn4(inputs)
+        inputs = f.relu(inputs)
+        inputs = self.bn3(inputs)
+        inputs = self.cnn5(inputs)
+        inputs = self.cnn6(inputs)
         inputs = f.relu(inputs)
         return inputs
