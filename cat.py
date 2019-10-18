57332f0eb01a68e063499e1c85d6a112a797717f
brycezxl <vincentlei@163.com>
2019-10-18 21:55:44 +0800

    wider cnn

--- cat.py
+++ cat.py
@@ -1,13 +1,12 @@
 from torchtext.data import BucketIterator
 
-from modules import *
 from utils import *
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 def main():
-    train, test, field = dataset_reader(train=True, stop=1200000)
+    train, test, field = dataset_reader(train=True, stop=900000)
     evl, _ = dataset_reader(train=False, fields=field)
     field.build_vocab(train, evl)
     _, evl_iter = BucketIterator.splits(
@@ -19,24 +18,22 @@ def main():
         sort=False
         )
 
-    with open('data/simple_rnn_result.txt', 'w+') as f:
+    with open('data/result.txt', 'w+') as f:
         f.write('')
 
-    with torch.no_grad():
-        for i, data in tqdm.tqdm(enumerate(evl_iter), total=evl_iter.__len__()):
-
-            day = outputs[6] * 3 + 3
-            hour = outputs[-1]
-
-            with open('data/simple_rnn_result.txt', 'a+') as f:
-                for b in range(day.size(0)):
-                    start_day = field.vocab.itos[data.create_time[b]][:-2]
-                    start_day = arrow.get(start_day).timestamp
-                    sign_day = int('%.0f' % day[b])
-                    sign_hour = ('%.0f' % (hour[b] * 5 + 15)).zfill(2)
-                    final = str(arrow.get(start_day + sign_day * 24 * 60 * 60))[:10]
-                    final = final + ' ' + sign_hour
-                    f.write(final + '\n')
+    with open('data/2.txt', "r") as f1:
+        with open('data/3.txt', "r") as f2:
+            for i, data in tqdm.tqdm(enumerate(evl_iter), total=evl_iter.__len__()):
+                with open('data/result.txt', 'a+') as f:
+                    for b in range(data.create_time.size(0)):
+                        start_day = field.vocab.itos[data.create_time[b]][:-2]
+                        start_day = arrow.get('2019-' + start_day).timestamp
+                        day = (float(f1.readline()) + float(f2.readline())) / 2
+                        sign_day = int('%.0f' % day)
+                        sign_hour = '15'
+                        final = str(arrow.get(start_day + sign_day * 24 * 60 * 60))[:10]
+                        final = final + ' ' + sign_hour
+                        f.write(final + '\n')
 
 
 if __name__ == '__main__':
