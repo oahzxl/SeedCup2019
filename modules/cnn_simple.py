946f45766336e79181d9f8c7d56f6ba4dae18209
brycezxl <vincentlei@163.com>
2019-10-18 17:40:15 +0800

    update

--- modules/cnn_simple.py
+++ modules/cnn_simple.py
@@ -10,7 +10,7 @@ class CNN(Module):
         self.cnn1 = nn.Conv1d(in_channels=12, out_channels=64, kernel_size=3, stride=2, padding=1)
         self.cnn2 = nn.Conv1d(in_channels=64, out_channels=64, kernel_size=3, stride=2, padding=1)
         self.bn2 = nn.BatchNorm1d(64)
-        self.cnn3 = nn.Conv1d(in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=1)
+        self.cnn3 = nn.Conv1d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
         self.cnn4 = nn.Conv1d(in_channels=128, out_channels=128, kernel_size=2, stride=1, padding=1)
         self.bn3 = nn.BatchNorm1d(128)
         self.cnn5 = nn.Conv1d(in_channels=128, out_channels=256, kernel_size=2, stride=1, padding=0)
@@ -25,10 +25,6 @@ class CNN(Module):
         inputs = self.cnn3(inputs)
         inputs = self.cnn4(inputs)
         inputs = f.relu(inputs)
-        inputs = self.bn3(inputs)
-        inputs = self.cnn5(inputs)
-        inputs = self.cnn6(inputs)
-        inputs = f.relu(inputs)
         return inputs
 
 
@@ -38,7 +34,7 @@ class SimpleCNN(Module):
         self.embedding = nn.Embedding(num_embeddings=num_embeddings, embedding_dim=embedding_dim)
         self.cnn = CNN()
 
-        self.fc_1 = nn.Linear(in_features=768, out_features=256)
+        self.fc_1 = nn.Linear(in_features=1152, out_features=256)
         self.fc_2 = nn.Linear(in_features=256, out_features=1)
         self.dropout = nn.Dropout(p=0.5)
 
